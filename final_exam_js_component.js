97c17189f74eebc52ac54978fcc22b54411ac153
tanaydixon <112428569+tanaydixon@users.noreply.github.com>
2022-12-16 14:01:36 -0500

    Add files via upload

---
+++ final_exam_js_component.js
@@ -0,0 +1,50 @@
+/*
+  Do NOT uncomment the lines of code below, they will cause errors.  
+  But I am providing it as a template.
+  */
+
+/* 
+document.querySelector("ELEMENT").addEventListener("EVENT",function(){
+     //YOUR CODE
+ })
+ */
+
+
+
+// Problem 1
+window.addEventListener("load", function() {
+  console.log("Chilli's")});
+
+// Problem 2
+let heading = document.querySelector('h1');
+
+heading.addEventListener("click", function(){
+heading.style.color= "#6061FF";
+heading.style.fontSize= " 50px ";
+})
+
+
+// Problem 3
+// Part 1
+let x = document.querySelectorAll("figcaption")[0];
+x.addEventListener('mouseover', function()
+{
+  x.style.border='solid blue 10px';
+})
+
+
+
+// Problem 3
+// Part 2
+
+
+
+// Problem 4
+
+
+
+// Problem 5
+
+
+// Problem 6
+
